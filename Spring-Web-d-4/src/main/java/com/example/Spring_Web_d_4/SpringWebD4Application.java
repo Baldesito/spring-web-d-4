75ef6c9f7f0a9abd28d42777ebc8aba492095d91
OCCHIO NERO <112170802+Baldesito@users.noreply.github.com>
2025-02-13 16:12:58 +0100

    first commit

---
+++ Spring-Web-d-4/src/main/java/com/example/Spring_Web_d_4/SpringWebD4Application.java
@@ -0,0 +1,13 @@
+package com.example.Spring_Web_d_4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebD4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebD4Application.class, args);
+	}
+
+}
